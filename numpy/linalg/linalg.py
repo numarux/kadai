80ad25f80d13288fa859e9f99f6f50072a3e59ae
Yuki Iinuma <cnj14008@ict.nitech.ac.jp>
2024-06-13 16:04:56 +0900

    modified

--- numpy/linalg/linalg.py
+++ numpy/linalg/linalg.py
@@ -1,16 +1,21 @@
-def __getattr__(attr_name):
-    import warnings
-    from numpy.linalg import _linalg
-    ret = getattr(_linalg, attr_name, None)
-    if ret is None:
-        raise AttributeError(
-            f"module 'numpy.linalg.linalg' has no attribute {attr_name}")
+import warnings
+from numpy.linalg import _linalg
+
+def __getattr__(name):
+    try:
+        return getattr(_linalg, name)
+    except AttributeError:
+        raise AttributeError(f"module 'numpy.linalg' has no attribute '{name}'")
     warnings.warn(
         "The numpy.linalg.linalg has been made private and renamed to "
         "numpy.linalg._linalg. All public functions exported by it are "
-        f"available from numpy.linalg. Please use numpy.linalg.{attr_name} "
+        f"available from numpy.linalg. Please use numpy.linalg.{name} "
         "instead.",
         DeprecationWarning,
-        stacklevel=3
+        stacklevel=2
     )
-    return ret
+
+# Assign the __getattr__ function to the numpy.linalg module
+import numpy.linalg as linalg
+linalg.__getattr__ = __getattr__
+
